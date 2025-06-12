a4b5ea51eeed260b5ac10b93355aa8a92e75d84c
paperclip <66579862+gitpaperclip@users.noreply.github.com>
2025-06-11 20:29:44 -0400

    elevator

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
@@ -4,8 +4,38 @@ import org.littletonrobotics.junction.AutoLog;
 
 public interface ElevatorIO {
  @AutoLog
- public static class ElevatorIOInputs {
-    
+ class ElevatorIOInputs {
+   public ElevatorIOData data = new ElevatorIOData(
+    false,
+    0.0,
+    0.0, 
+    0.0, 
+    0.0, 
+    0.0, 
+    0.0
+    );
  }
+    record ElevatorIOData(
+        boolean motorConnected,
+        double positionRad,
+        double velocityRadPerSec,
+        double appliedVolts,
+        double torqueCurrentAmps,
+        double supplyCurrentAmps,
+        double tempCelsius
+    ) {}
     
+    default void updateInputs(ElevatorIOInputs inputs) {}
+
+    default void runOpenLoop(double output) {}
+  
+    default void runVolts(double volts) {}
+  
+    default void stop() {}
+  
+    default void runPosition(double positionRad, double feedforward) {}
+  
+    default void setPID(double kP, double kI, double kD) {}
+  
+    default void setBrakeMode(boolean enabled) {}
 } 
