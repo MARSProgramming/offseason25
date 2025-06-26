66b7ad75ddde29230fb7f9c2496dd47c0b42ecff
paperclip <66579862+gitpaperclip@users.noreply.github.com>
2025-06-26 10:38:29 -0700

    refresh commit

---
+++ src/main/java/frc/robot/subsystems/coral/CoralIO.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems.coral;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface CoralIO {
+  @AutoLog
+  class CoralIOInputs {
+    public CoralIOData data = new CoralIOData( 0.0, 0.0, 0.0);
+  }
+
+  record CoralIOData(
+      double motorOutputPercent,
+      double IrSensorValue,
+      double motorStatorCurrent) {}
+
+  default void updateInputs(CoralIOInputs inputs) {}
+
+  default void setOutput(double percent) {}
+
+  default void setMode() {}
+
+  default void setIRThreshold(double value) {}
+}
